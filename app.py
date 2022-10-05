e47d4c5b7ab9305f171d01164ad4f441b252d96d
Sidney Júnior <sidneyrj45@gmail.com>
2022-10-05 13:25:05 -0300

    arquivo json adicionado

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_json('data.json')
+Produtos = df.to_dict('records')
 
 @app.route('/')
 def index():
@@ -13,6 +17,10 @@ def teste():
 @app.route('/produtos')
 def produtos():
     return render_template('produtos.html')
+
+@app.route('/teste_produtos')
+def teste_produtos():
+    return render_template('teste_produto.html', produtos = Produtos)
 
 if __name__ == '__main__':
     app.run(debug=True)
