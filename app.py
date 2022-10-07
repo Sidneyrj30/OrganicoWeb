e355e2d638a4c3f9ec7e51d75bc1272d3756c5cf
bruno <brunocosta@luizalabs.com>
2022-10-07 14:34:14 -0300

    Programa cadastro e exclusão funcionando.
    realizado testes e esta funcional

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
 import pandas as pd
 
 app = Flask(__name__)
 
 df = pd.read_json('data.json')
-Produtos = df.to_dict('records')
+produtos = df.to_dict('records')
 
 @app.route('/')
 def index():
@@ -14,13 +14,33 @@ def index():
 def teste():
     return render_template('teste.html')
 
-@app.route('/produtos')
-def produtos():
-    return render_template('produtos.html')
+@app.route('/listar_produtos')
+def listar():
+    global df
+    df = pd.read_json('data.json')
+    return render_template('listar_produtos.html', produtos = df.to_dict('records') )
 
-@app.route('/teste_produtos')
-def teste_produtos():
-    return render_template('teste_produto.html', produtos = Produtos)
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastrado')#/adicionar?produto=valor&preco=valor
+def cadastrado():
+    global df
+    argumentos = request.args.to_dict(True)
+    print(argumentos)
+    argumentos['id'] = [df['id'].max() + 1]
+    df = pd.concat([df, pd.DataFrame(argumentos)], ignore_index=True)
+    df.to_json('data.json', orient='records')
+    return redirect('/cadastro')
+
+@app.route('/deletar/<id>')
+def deletar(id):
+    mask_id = df[df['id'] == int(id)].index
+    df.drop(mask_id, axis=0, inplace=True)
+    df.to_json('data.json', orient='records')
+    return redirect('/listar_produtos')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
